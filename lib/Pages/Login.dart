165416b308bb4994b81d9ccb6f2b6fc3c9c1f45f
7iilko <arabradui170@gmail.com>
2022-12-21 01:32:53 +0200

    emailadress

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -10,18 +10,26 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(33.0),
           child: Column(
             children: [
-              const SizedBox(height: 64,),
+              const SizedBox(
+                height: 64,
+              ),
               TextField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
                 decoration: InputDecoration(
-                  hintText: "Enter Your Password : ",
+                  hintText: "Enter Your Email : ",
                   // To delete borders
-                  enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: Divider.createBorderSide(context),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
                   // fillColor: Colors.red,
                   filled: true,
                   contentPadding: const EdgeInsets.all(8),
