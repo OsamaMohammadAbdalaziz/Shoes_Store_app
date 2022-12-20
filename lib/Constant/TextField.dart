3b3b48f733cd8f68f75fbf4aa67620d6aac58a0c
7iilko <arabradui170@gmail.com>
2022-12-21 01:50:51 +0200

    Create folder(TextField)__and page

---
+++ lib/Constant/TextField.dart
@@ -0,0 +1,18 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      
+    );
+  }
+}
