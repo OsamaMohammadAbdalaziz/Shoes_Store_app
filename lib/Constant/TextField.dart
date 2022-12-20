3af59797169600c4b558f30a43a25c7f855669a6
7iilko <arabradui170@gmail.com>
2022-12-21 01:58:39 +0200

    using properties TextField

--- lib/Constant/TextField.dart
+++ lib/Constant/TextField.dart
@@ -1,16 +1,28 @@
-
-
-
-
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({Key?key}) :super (key: key);
+  const MyTextField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-
+      keyboardType: TextInputType.emailAddress,
+      obscureText: false,
+      decoration: InputDecoration(
+        hintText: "Enter Your Email : ",
+        //  To delete borders
+        enabledBorder: OutlineInputBorder(
+          borderSide: Divider.createBorderSide(context),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+        // fillColor: Colors.red,
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+      ),
     );
   }
 }
