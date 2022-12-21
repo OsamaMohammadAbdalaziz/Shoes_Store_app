c75b3daf08997b808353cd546bc7b1d323fccef0
7iilko <arabradui170@gmail.com>
2022-12-21 23:43:00 +0200

    Reusable Widget-Constant page

--- lib/Shared/Constant.dart
+++ lib/Shared/Constant.dart
@@ -1,39 +1,19 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
-class Constant extends StatelessWidget {
-  final TextInputType textInputTypeee;
-  final bool isPassword;
-  final String hinttexttt;
 
-  Constant({
-    Key? key,
-    required this.textInputTypeee,
-    required this.isPassword,
-    required this.hinttexttt,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      keyboardType: textInputTypeee,
-      obscureText: isPassword,
-      decoration: InputDecoration(
-        hintText: hinttexttt,
-        // To delete borders
-        enabledBorder: OutlineInputBorder(
-          borderSide: Divider.createBorderSide(context),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.grey,
-          ),
-        ),
-        // fillColor: Colors.red,
-        filled: true,
-        contentPadding: const EdgeInsets.all(8),
-      ),
-    );
-  }
-}
+const aaaa = InputDecoration(
+  hintText: "Enter Your Email : ",
+  // To delete borders
+  enabledBorder: OutlineInputBorder(
+borderSide: BorderSide.none,
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+color: Colors.grey,
+    ),
+  ),
+  // fillColor: Colors.red,
+  filled: true,
+  contentPadding: const EdgeInsets.all(8),
+);
