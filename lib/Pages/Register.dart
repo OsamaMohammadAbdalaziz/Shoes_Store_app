840eb9586b2eefd42d3891278a8737be0b3d7780
7iilko <arabradui170@gmail.com>
2022-12-22 19:28:12 +0200

    background,body

--- lib/Pages/Register.dart
+++ lib/Pages/Register.dart
@@ -1,6 +1,8 @@
 
 
 
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Register extends StatelessWidget {
@@ -10,7 +12,12 @@ class Register extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+          ),
+        ),
       ),
     );
   }
