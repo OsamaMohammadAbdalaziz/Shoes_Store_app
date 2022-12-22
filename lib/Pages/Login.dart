20fd8a27626e2c97b44fd0990c7be5d7a0979319
7iilko <arabradui170@gmail.com>
2022-12-22 18:36:21 +0200

    onPressed,child in TextButton

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -59,6 +59,10 @@ class Login extends StatelessWidget {
                 children: [
                   Text("Do not have account ?",
                   style: TextStyle(fontSize: 18)),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: Text("Sign up",
+                    style: TextStyle(color: Colors.black, fontSize: 18))),
                 ],
               ),
             ],
