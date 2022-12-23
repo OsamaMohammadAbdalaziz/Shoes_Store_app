9915072809ca68fd254000c1b9a7235b72c829dd
7iilko <arabradui170@gmail.com>
2022-12-23 17:47:35 +0200

    use body

--- lib/Pages/Product_details.dart
+++ lib/Pages/Product_details.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Details extends StatelessWidget {
-  const Details({Key? key}) :super(key: key);
+  const Details({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold()
-      );
+        child: Scaffold(
+      body: Text("Details"),
+    ));
   }
 }
