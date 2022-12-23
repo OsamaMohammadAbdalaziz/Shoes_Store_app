fdabcdcd3e4fe011bd494cdfaab3ca571a7f8146
7iilko <arabradui170@gmail.com>
2022-12-23 20:58:16 +0200

    required this.name

--- lib/Model/Item.dart
+++ lib/Model/Item.dart
@@ -1,17 +1,54 @@
 class Item {
-  String Location;
+  String name;
+  String location;
   String imagepath;
   double price;
-  Item({required this.imagepath, required this.price, required this.Location});
+  Item(
+      {required this.imagepath,
+      required this.price,
+      required this.location,
+      required this.name});
 }
 
 final List<Item> items = [
-    Item(imagepath: "assets/image/1.webp", price: 90, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/2.webp", price: 80, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/3.webp", price: 100, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/4.webp", price: 150, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/5.webp", price: 125, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/6.webp", price: 165, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/7.webp", price: 180, Location: "Outlet Store"),
-    Item(imagepath: "assets/image/8.webp", price: 200, Location: "Outlet Store"),
-  ];
+  Item(
+      name: "product1",
+      imagepath: "assets/image/1.webp",
+      price: 90,
+      location: "Outlet Store"),
+  Item(
+      name: "product2",
+      imagepath: "assets/image/2.webp",
+      price: 80,
+      location: "Outlet Store"),
+  Item(
+      name: "product3",
+      imagepath: "assets/image/3.webp",
+      price: 100,
+      location: "Outlet Store"),
+  Item(
+      name: "product4",
+      imagepath: "assets/image/4.webp",
+      price: 150,
+      location: "Outlet Store"),
+  Item(
+      name: "product5",
+      imagepath: "assets/image/5.webp",
+      price: 125,
+      location: "Outlet Store"),
+  Item(
+      name: "product6",
+      imagepath: "assets/image/6.webp",
+      price: 165,
+      location: "Outlet Store"),
+  Item(
+      name: "product6",
+      imagepath: "assets/image/7.webp",
+      price: 180,
+      location: "Outlet Store"),
+  Item(
+      name: "product7",
+      imagepath: "assets/image/8.webp",
+      price: 200,
+      location: "Outlet Store"),
+];
