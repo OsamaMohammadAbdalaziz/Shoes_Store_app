3a668beaa17fae3f6adf5956e33c612ed8f672cc
7iilko <arabradui170@gmail.com>
2022-12-24 01:44:42 +0200

    appBar

--- lib/Pages/CheckOut.dart
+++ lib/Pages/CheckOut.dart
@@ -1,16 +1,17 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shoes_store_app/Provider/Cart.dart';
 
 class CheckOut extends StatelessWidget {
-  const CheckOut({Key? key}) :super(key: key);
+  const CheckOut({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final carttt = Provider.of<Cart>(context);
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Check Out"),
+      ),
+    );
   }
 }
