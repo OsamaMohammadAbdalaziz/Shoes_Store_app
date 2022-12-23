922a446f135bdabf099264b6acaa6ce73610e337
7iilko <arabradui170@gmail.com>
2022-12-24 01:40:05 +0200

    import ProductsAndPrice in Details page

--- lib/Pages/Product_details.dart
+++ lib/Pages/Product_details.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:shoes_store_app/Model/Item.dart';
+import 'package:shoes_store_app/Shared/AppBar.dart';
 import 'package:shoes_store_app/Shared/Colors.dart';
 
 class Details extends StatefulWidget {
@@ -21,43 +22,7 @@ class _DetailsState extends State<Details> {
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    bottom: 24,
-                    child: Container(
-                      child: Text(
-                        "8",
-                        style: TextStyle(
-                          color: BTNblue,
-                          fontSize: 16,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          shape: BoxShape.circle),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.add_shopping_cart),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 12),
-                child: Text(
-                  "\$80",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
-          )
-        ],
+        actions: [ProductsAndPrice()],
         backgroundColor: appbarBlue,
         title: Text("Product details"),
       ),
