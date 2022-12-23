43ff9aa910b92421c1fcb69d6575a18b7804b116
7iilko <arabradui170@gmail.com>
2022-12-24 01:48:17 +0200

    backgroundColor AppBar

--- lib/Pages/CheckOut.dart
+++ lib/Pages/CheckOut.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shoes_store_app/Provider/Cart.dart';
 import 'package:shoes_store_app/Shared/AppBar.dart';
+import 'package:shoes_store_app/Shared/Colors.dart';
 
 class CheckOut extends StatelessWidget {
   const CheckOut({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class CheckOut extends StatelessWidget {
     final carttt = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: appbarBlue,
         title: Text("Check Out"),
         actions: [ProductsAndPrice()],
       ),
