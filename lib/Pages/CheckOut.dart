90d65c21cee75b54298c6b3300358317ebda3172
7iilko <arabradui170@gmail.com>
2022-12-24 01:48:48 +0200

    ignore for file

--- lib/Pages/CheckOut.dart
+++ lib/Pages/CheckOut.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shoes_store_app/Provider/Cart.dart';
