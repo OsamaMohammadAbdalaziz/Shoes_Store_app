dc7f617fc0e135dd07526740f922c1e91085f19d
7iilko <arabradui170@gmail.com>
2022-12-23 13:27:39 +0200

    appBar/title

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -8,6 +8,10 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+    );
   }
 }
