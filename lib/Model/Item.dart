d0d52a4fabe74709e0c737af2ce5fde344ccab35
7iilko <arabradui170@gmail.com>
2022-12-23 20:41:12 +0200

    change images 3,4 becuase the images are skewed

--- lib/Model/Item.dart
+++ lib/Model/Item.dart
@@ -7,8 +7,8 @@ class Item {
 final List<Item> items = [
     Item(imagepath: "assets/image/1.webp", price: 80),
     Item(imagepath: "assets/image/2.webp", price: 80),
-    Item(imagepath: "assets/image/3.jpg", price: 80),
-    Item(imagepath: "assets/image/4.jpg", price: 80),
+    Item(imagepath: "assets/image/3.webp", price: 80),
+    Item(imagepath: "assets/image/4.webp", price: 80),
     Item(imagepath: "assets/image/5.webp", price: 80),
     Item(imagepath: "assets/image/6.webp", price: 80),
     Item(imagepath: "assets/image/7.webp", price: 80),
