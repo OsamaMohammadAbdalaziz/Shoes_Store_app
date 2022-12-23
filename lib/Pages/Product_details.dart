ba1b19229bfa52f71c1f07814d665e01e35d2245
7iilko <arabradui170@gmail.com>
2022-12-23 18:40:27 +0200

    size/color/color comment

--- lib/Pages/Product_details.dart
+++ lib/Pages/Product_details.dart
@@ -112,11 +112,16 @@ class Details extends StatelessWidget {
               const SizedBox(
                 width: 66,
               ),
-            Row(
-              children: [
-                Icon(Icons.edit_location,),
-              ],
-            ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.edit_location,
+                    size: 26,
+                    color: Color.fromARGB(168, 3, 65, 27),
+                    // color: Color.fromARGB(255, 186, 30, 30),
+                  ),
+                ],
+              ),
             ],
           )
         ],
