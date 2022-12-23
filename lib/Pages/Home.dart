4ca7776a4ced4f2aaf40738217135e4c78afa9c0
7iilko <arabradui170@gmail.com>
2022-12-23 14:35:58 +0200

    wrap with SafeArea

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -8,41 +8,46 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    child: Text(
-                      "8",
-                      style: TextStyle(fontSize: 18, color: BTNblue),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: BTNblue,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          shape: BoxShape.circle),
                     ),
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        shape: BoxShape.circle),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.add_shopping_cart),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: Text(
-                  "\$ 40",
-                  style: TextStyle(fontSize: 18),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          )
-        ],
-        backgroundColor: appbarBlue,
-        title: Text("Home"),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text(
+                    "\$ 40",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            )
+          ],
+          backgroundColor: appbarBlue,
+          title: Text("Home"),
+        ),
       ),
     );
   }
