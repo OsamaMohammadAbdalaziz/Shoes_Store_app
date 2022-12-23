327c2caf1ab8b1860104630b3a7105eafa5d5f97
7iilko <arabradui170@gmail.com>
2022-12-24 01:35:54 +0200

    StatelessWidget/import : material,provider,Provider/Cart and Colors

--- lib/Shared/AppBar.dart
+++ lib/Shared/AppBar.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shoes_store_app/Provider/Cart.dart';
+import 'package:shoes_store_app/Shared/Colors.dart';
 
-class AppBar extends StatelessWidget {
-  const AppBar({super.key});
+class ProductsAndPrice extends StatelessWidget {
+  const ProductsAndPrice({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    final carttt = Provider.of<Cart>(context);
+    return Row(
+      children: [
+        Stack(
+          children: [
+            Positioned(
+              bottom: 24,
+              child: Container(
+                child: Text(
+                  "${carttt.SelectedProducts.length}",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: BTNblue,
+                  ),
+                ),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    shape: BoxShape.circle),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add_shopping_cart),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 12),
+          child: Text(
+            "\$ ${carttt.Price}",
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
+    );
   }
 }
