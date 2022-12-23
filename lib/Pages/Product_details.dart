f8d1f1b493964996d39f3b88489d6408a5f12e28
7iilko <arabradui170@gmail.com>
2022-12-23 19:04:02 +0200

    SingleChildScrollView/Text

--- lib/Pages/Product_details.dart
+++ lib/Pages/Product_details.dart
@@ -51,102 +51,110 @@ class Details extends StatelessWidget {
         backgroundColor: appbarBlue,
         title: Text("Product details"),
       ),
-      body: Column(
-        children: [
-          Image.asset("assets/image/1.webp"),
-          const SizedBox(
-            height: 11,
-          ),
-          Text(
-            "\$80",
-            style: TextStyle(fontSize: 20),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  "New",
-                  style: TextStyle(fontSize: 15),
-                ),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 255, 129, 129),
-                  borderRadius: BorderRadius.circular(4),
-                ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 66,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.edit_location,
-                    size: 26,
-                    color: Color.fromARGB(168, 3, 65, 27),
-                    // color: Color.fromARGB(255, 186, 30, 30),
-                  ),
-                  const SizedBox(
-                    width: 3,
-                  ),
-                  Text(
-                    "Shoes Shop",
-                    style: TextStyle(fontSize: 19),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              "Details : ",
-              style: TextStyle(fontSize: 22),
-              textAlign: TextAlign.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/image/1.webp"),
+            const SizedBox(
+              height: 11,
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-        ],
+            Text(
+              "\$80",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    "New",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 129, 129),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 66,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.edit_location,
+                      size: 26,
+                      color: Color.fromARGB(168, 3, 65, 27),
+                      // color: Color.fromARGB(255, 186, 30, 30),
+                    ),
+                    const SizedBox(
+                      width: 3,
+                    ),
+                    Text(
+                      "Shoes Shop",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Details : ",
+                style: TextStyle(fontSize: 22),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              "A shoe is an item of footwear intended to protect and comfort the human foot. They are often worn with a sock. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with form originally being tied to function. Though the human foot can adapt to varied terrains and climate conditions, it is still vulnerable to environmental hazards such as sharp rocks and temperature extremes, which shoes protect against. Some shoes are worn as safety equipment, such as steel-toe boots which are required footwear at industrial worksites.",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
