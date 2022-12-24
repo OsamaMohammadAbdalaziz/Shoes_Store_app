0b5cf0bdaa4b473b17d2bff17fbe426519c6f012
7iilko <arabradui170@gmail.com>
2022-12-24 02:45:55 +0200

    ignore for file

--- lib/Provider/Cart.dart
+++ lib/Provider/Cart.dart
@@ -1,20 +1,20 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:shoes_store_app/Model/Item.dart';
 
 class Cart with ChangeNotifier {
+  List SelectedProducts = [];
+  int Price = 0;
+  add(Item product) {
+    SelectedProducts.add(product);
+    Price += product.price.round();
+    notifyListeners();
+  }
 
-List SelectedProducts = [];
-int Price =0;
-add(Item product){
-  SelectedProducts.add(product);
-  Price += product.price.round();
-  notifyListeners();
-}
-delete(Item product){
-  SelectedProducts.remove(product);
-  Price -= product.price.round();
-  notifyListeners();
-}
-
-
+  delete(Item product) {
+    SelectedProducts.remove(product);
+    Price -= product.price.round();
+    notifyListeners();
+  }
 }
